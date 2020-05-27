26f44911c91ba74de2dab466f58f526f625996db
Nathanaël <contact@nathanaelhoun.fr>
2020-05-27 12:15:46 +0200

    Comparison should ignore accents (#10)

--- server/plugin.go
+++ server/plugin.go
@@ -3,6 +3,11 @@ package main
 import (
 	"net/http"
 	"strings"
+	"unicode"
+
+	"golang.org/x/text/runes"
+	"golang.org/x/text/transform"
+	"golang.org/x/text/unicode/norm"
 
 	"github.com/mattermost/mattermost-server/model"
 	"github.com/mattermost/mattermost-server/plugin"
@@ -23,7 +28,7 @@ func main() {
 func (p *Plugin) OnActivate() error {
 	p.badWords = make(map[string]bool, len(badWords))
 	for _, word := range badWords {
-		p.badWords[strings.ToLower(word)] = true
+		p.badWords[strings.ToLower(removeAccents(word))] = true
 	}
 
 	return nil
@@ -37,7 +42,7 @@ func (p *Plugin) ServeHTTP(c *plugin.Context, w http.ResponseWriter, r *http.Req
 }
 
 func (p *Plugin) WordIsBad(word string) bool {
-	_, ok := p.badWords[strings.ToLower(word)]
+	_, ok := p.badWords[strings.ToLower(removeAccents(word))]
 	return ok
 }
 
@@ -63,4 +68,14 @@ func (p *Plugin) MessageWillBePosted(c *plugin.Context, post *model.Post) (*mode
 
 func (p *Plugin) MessageWillBeUpdated(c *plugin.Context, newPost *model.Post, _ *model.Post) (*model.Post, string) {
 	return p.FilterPost(newPost)
+}
+
+func removeAccents(s string) string {
+	t := transform.Chain(norm.NFD, runes.Remove(runes.In(unicode.Mn)), norm.NFC)
+	output, _, e := transform.String(t, s)
+	if e != nil {
+		return s
+	}
+
+	return output
 }
